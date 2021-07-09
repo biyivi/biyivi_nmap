a9a725279968f0bb048e567d761e10e997cc324b
biyivi <75817113+biyivi@users.noreply.github.com>
2021-07-08 21:18:42 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+echo "Iniciando  instalacion..."
+sleep 2 
+pkg install python -y
+clear
+pip install colorama
+clear
+pip install tqdm
+clear
+echo "La instalacion se ha completado.."
+var1="1"
+echo "Iniciar script"
+echo "1) Si"
+echo "2) Salir"
+read -p ">> " resp
+if [ "$resp" == "$var1" ]
+then
+python nmap_bgnnrs.py
+else
+echo "Para iniciar el scripit debe escribir: python nmap_bgnnrs.py"
+echo ":D"
+fi
